1ff317443bb93f14f3ecde874697e0610a838b0d
danimontesano <daniel.mon1234@gmail.com>
2022-06-18 02:49:30 -0300

    log-in 1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { LogInComponent } from './log-in/log-in.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'log-in', component: LogInComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
